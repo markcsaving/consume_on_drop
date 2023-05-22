fa114380f989d207023be76f2c5f3585d4506105
Mark Saving <marksaving@gmail.com>
2023-05-22 18:52:28 -0500

    Added tests, implemented Consume for FnOnce() -> ()

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![no_std]
+#[warn(missing_docs)]
+
+extern crate alloc;
 
 /// This trait is for types with a specified means of consumption.
 /// It is a counterpart to [`Drop`]. While [`Drop::drop`] takes `self`
@@ -12,13 +15,19 @@ pub trait Consume {
     fn consume(self);
 }
 
+impl<T: FnOnce()> Consume for T {
+    fn consume(self) {
+        self()
+    }
+}
+
 pub use crate::consume_on_drop::*;
 pub use crate::with_consumer::*;
 
 mod consume_on_drop {
+    use super::Consume;
     use core::mem::ManuallyDrop;
     use core::ops::{Deref, DerefMut};
-    use super::Consume;
 
     /// A zero-overhead wrapper around `T`. When a [`ConsumeOnDrop<T>`] is dropped,
     /// the underlying `T` is [`Consume::consume`]d.
@@ -32,7 +41,9 @@ mod consume_on_drop {
         /// Wraps a `T` in a [`ConsumeOnDrop`].
         #[inline]
         pub const fn new(value: T) -> Self {
-            Self { inner: ManuallyDrop::new(value) }
+            Self {
+                inner: ManuallyDrop::new(value),
+            }
         }
 
         /// Unwraps the underlying `T`.
@@ -79,9 +90,9 @@ mod consume_on_drop {
 // Note: this module doesn't use the "unsafe" keyword. It's purely
 // a safe abstraction on top of the `consume_on_drop` module.
 mod with_consumer {
-    use core::ops::{Deref, DerefMut};
-    use crate::Consume;
     use super::ConsumeOnDrop;
+    use crate::Consume;
+    use core::ops::{Deref, DerefMut};
 
     /// A type implementing [`Consumer<T>`] is one which can consume a value
     /// of type `T`. In particular, any `FnOnce(T)` is also a [`Consumer<T>`].
@@ -122,8 +133,7 @@ mod with_consumer {
         #[inline]
         pub const fn new(val: T, cons: Q) -> Self {
             Self {
-                inner: ConsumeOnDrop::new(
-                    RawWithConsumer(val, cons))
+                inner: ConsumeOnDrop::new(RawWithConsumer(val, cons)),
             }
         }
 
@@ -171,5 +181,85 @@ mod with_consumer {
         fn deref_mut(&mut self) -> &mut Self::Target {
             Self::as_muts(self).0
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Consume, ConsumeOnDrop, Consumer, WithConsumer};
+    use alloc::string::{String, ToString};
+    use alloc::vec::Vec;
+    use core::mem::{size_of, size_of_val};
+    use core::ops::{Deref, DerefMut};
+    use core::sync::atomic::{AtomicUsize, Ordering};
+
+    #[test]
+    fn basic_consume() {
+        let mut i = 0;
+        {
+            let mut z = ConsumeOnDrop::new(|| i += 1);
+            z.deref_mut()(); // i is now 1
+            assert_eq!(size_of_val(&z), size_of::<&mut i32>());
+        } // z dropped, i is now 2
+        assert_eq!(i, 2);
+        {
+            let z = WithConsumer::new((), |()| i += 1);
+            WithConsumer::into_inner(z);
+        }
+        assert_eq!(i, 2);
+    }
+
+    #[test]
+    fn custom_consumer() {
+        struct Pusher<'a>(&'a mut Vec<String>);
+
+        impl<'a> Consumer<String> for Pusher<'a> {
+            fn consume(self, other: String) {
+                self.0.push(other)
+            }
+        }
+
+        let mut vector = Vec::new();
+
+        let string = WithConsumer::new("hello".into(), Pusher(&mut vector));
+        assert_eq!(string.deref(), "hello");
+        drop(string);
+        assert_eq!(&vector, &["hello".to_string()]);
+
+        let mut vec2 = Vec::new();
+        let mut string = WithConsumer::new("Hello".to_string(), Pusher(&mut vector));
+        // We can switch out the consumer as long as it's the same type.
+        *WithConsumer::as_muts(&mut string).1 = Pusher(&mut vec2);
+        // We can use `string` as if it were a `String`.
+        string.extend(" world!".chars());
+        drop(string);
+        assert_eq!(&vector, &["hello".to_string()]);
+        assert_eq!(&vec2, &["Hello world!".to_string()]);
+    }
+
+    /// See [this question](https://stackoverflow.com/questions/53254645/how-can-i-move-a-value-out-of-the-argument-to-dropdrop).
+    #[test]
+    fn stack_overflow_question_test() {
+
+        static COUNT: AtomicUsize = AtomicUsize::new(0);
+        struct T;
+
+        struct S {
+            _member: ConsumeOnDrop<T>,
+        }
+
+        fn destroy_t(t: T) {
+            COUNT.fetch_add(1, Ordering::Relaxed);
+        }
+
+        impl Consume for T {
+            fn consume(self) {
+                destroy_t(self)
+            }
+        }
+
+        let s = S { _member: ConsumeOnDrop::new(T) };
+        drop(s);
+        assert_eq!(COUNT.load(Ordering::Relaxed), 1);
     }
 }
